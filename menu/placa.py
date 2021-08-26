68a64285ef3e110a9d043ab6a4b8de8955961b22
Lursy <70035797+Lursy@users.noreply.github.com>
2021-08-26 00:15:14 -0300

    Update placa.py

--- menu/placa.py
+++ menu/placa.py
@@ -18,19 +18,30 @@ def cplaca():
             print(f'{ve}Limite de consulta atingido')
         else:
             print(f'''{am}Ano: {vd}{info['ano']}
-    {am}Ano modelo: {vd}{info['anoModelo']}
-    {am}Marca: {vd}{info['marca']}
-    {am}Cor: {vd}{info['cor']}
-    {am}Placa:  {vd}{info['placa']}
-    {am}Estado: {vd}{estado['nome']} ➤ {info['uf']}
-    {am}Cidade: {vd}{info['municipio']}
+{am}Ano modelo: {vd}{info['anoModelo']}
+{am}Marca: {vd}{info['marca']}
+{am}Cor: {vd}{info['cor']}
+{am}Placa:  {vd}{info['placa']}
+{am}Estado: {vd}{estado['nome']} ➤ {info['uf']}
+{am}Cidade: {vd}{info['municipio']}
             ''')
         print(f'{ve}━' * 43)
-        voltar = str(input(f'{am}Voltar{cy}[S/N]: {vd}')).lower()
-        if 's' in voltar[0]:
+        print(f'{am}[1] {cy}Menu\n'
+              f'{am}[2] {cy}Consultar novamente\n'
+              f'{am}[3] {cy}Sair')
+        voltar = str(input(f'{am}//: {vd}'))
+        if '1' in voltar[0]:
+            clear()
+        elif '2' in voltar[0]:
             clear()
             cplaca()
-        elif 'n' in voltar[0]:
+        elif '3' in voltar[0]:
+            clear()
+            print(f'{vd}Saindo...')
+            exit()
+        else:
+            print(f'{ve}Comando não identificado')
+            sleep(2)
             clear()
     else:
         print(f'{ve}Placa não identificada')
